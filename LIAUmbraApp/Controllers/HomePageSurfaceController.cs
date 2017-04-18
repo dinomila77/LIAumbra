83a8b36cbbe5ac71e07185347774382f3feb098f
Michel Miladinovic <miladino77@hotmail.com>
2017-04-18 11:30:20 +0200

    Adds controller

---
+++ LIAUmbraApp/Controllers/HomePageSurfaceController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace LIAUmbraApp.Controllers
+{
+    public class HomePageSurfaceController : SurfaceController
+    {
+        // GET: HomePageSurface
+        public ActionResult Index()
+        {
+            return CurrentUmbracoPage();
+        }
+    }
+}
