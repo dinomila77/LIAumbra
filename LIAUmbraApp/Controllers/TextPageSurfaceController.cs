f8b961edebfa9f2386899ec0bdaa364fbe538e3f
Michel Miladinovic <miladino77@hotmail.com>
2017-04-27 10:30:54 +0200

    Includes TextPage in project & adds surface controller

---
+++ LIAUmbraApp/Controllers/TextPageSurfaceController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace LIAUmbraApp.Controllers
+{
+    public class TextPageSurfaceController : SurfaceController
+    {
+        // GET: TextPageSurface
+        public ActionResult Index()
+        {
+            return CurrentUmbracoPage();
+        }
+    }
+}
