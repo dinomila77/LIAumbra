b8f04b61aa7046681f4e602993bfd1d95e8fa064
Michel Miladinovic <miladino77@hotmail.com>
2017-04-28 13:19:15 +0200

    Creates TextPage, parent node hard coded

--- LIAUmbraApp/Controllers/CreateContentSurfaceController.cs
+++ LIAUmbraApp/Controllers/CreateContentSurfaceController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using LIAUmbraApp.Models;
+using Umbraco.Web;
 using Umbraco.Web.Mvc;
 
 namespace LIAUmbraApp.Controllers
@@ -21,7 +22,32 @@ namespace LIAUmbraApp.Controllers
         public ActionResult SubmitForm(ContactViewModel model)
         {
             if (!ModelState.IsValid) return CurrentUmbracoPage();
-            
+
+            //var umb = new UmbracoVirtualNodeByIdRouteHandler(1000);
+
+            try
+            {
+                var contentService = ApplicationContext.Services.ContentService;
+                //var contentService2 = ApplicationContext.Current.Services.ContentService;
+                var parent = contentService.GetById(2076);
+
+                var newContent = contentService.CreateContent("New Node", parent, "textPage");
+                newContent.SetValue("bodyText","Hello");
+
+                var result = contentService.SaveAndPublishWithStatus(newContent);
+
+                if (result.Success)
+                {
+                    TempData["success"] = "Thank you!";
+                    return RedirectToCurrentUmbracoPage();
+                }
+            }
+            catch (Exception ex)
+            {
+                var error = $"{ex.Message}";
+                ModelState.AddModelError("Error", error);
+                return CurrentUmbracoPage();
+            }
 
             return CurrentUmbracoPage();
         }
