3f8a8c9bdc946d80959e3085d981b6b3f6eef87a
Michel Miladinovic <miladino77@hotmail.com>
2017-04-28 09:50:23 +0200

    Adds controller for CreateContent

---
+++ LIAUmbraApp/Controllers/CreateContentSurfaceController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace LIAUmbraApp.Controllers
+{
+    public class CreateContentSurfaceController : SurfaceController
+    {
+        // GET: CreateContentSurface
+        public ActionResult Index()
+        {
+            return CurrentUmbracoPage();
+        }
+    }
+}
