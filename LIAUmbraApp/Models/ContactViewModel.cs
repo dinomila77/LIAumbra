0d1b872a6d62347c623b03310a81feafb19e49cb
Michel Miladinovic <miladino77@hotmail.com>
2017-05-03 16:44:08 +0200

    Adds custom validation 4 ContactViewModel w/ Umbraco Dictionary translations

--- LIAUmbraApp/Models/ContactViewModel.cs
+++ LIAUmbraApp/Models/ContactViewModel.cs
@@ -3,18 +3,34 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using Umbraco.Web;
 
 namespace LIAUmbraApp.Models
 {
-    public class ContactViewModel
+    public class ContactViewModel : IValidatableObject
     {
-        [Required]
+        private readonly UmbracoHelper _umbHelper = new UmbracoHelper();
+
         public string FirstName { get; set; }
-
-        [Required]
         public string LastName { get; set; }
-
-        [Required]
         public string Message { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (FirstName == null)
+            {
+                yield return new ValidationResult($"{_umbHelper.GetDictionaryValue("firstNameRequired")}");
+            }
+
+            if (LastName == null)
+            {
+                yield return new ValidationResult($"{_umbHelper.GetDictionaryValue("lastNameRequired")}");
+            }
+
+            if (Message == null)
+            {
+                yield return new ValidationResult($"{_umbHelper.GetDictionaryValue("messageRequired")}");
+            }
+        }
     }
 }
